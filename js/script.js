f3f2256bc5e0259107229479bf08d4e39e9b45cf
Radek Bubienko <radek@bubienko.pl>
2022-10-22 21:00:29 +0200

    add and remove task,

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@
 
     ];
 
+    const addNewTask = (newTaskContent) => {
+      tasks.push({
+        content: newTaskContent,
+      });
+      render();
+    };
+
+    const removeTask = (taskIndex) => {
+      tasks.splice(taskIndex, 1);
+            render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -18,30 +30,42 @@
             htmlString += `
               <li>
                 ${task.content}
+                <button class="js-remove">usuń</button>
               </li>  
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+          removeButton.addEventListener("click", () => {
+            removeTask(index);
+          });
+        });
+    };
+
+    
+
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+        return;
+      }
+
+      addNewTask(newTaskContent);
     };
 
     const init = () => {
         render();
 
-        const form = addEventListener("submit", (event) => {
-          event.preventDefault();
+        const form = document.querySelector(".js-form");
 
-          const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-          if (newTaskContent === "") {
-            return;
-          }
-
-          tasks.push({
-            content: newTaskContent,
-          });
-          render();
-        });
+        form.addEventListener("submit", onFormSubmit);
 
     };
 
