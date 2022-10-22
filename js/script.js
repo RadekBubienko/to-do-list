14c2764d1f85979ba0d2738bbf2fc5d15c6794be
Radek Bubienko <radek@bubienko.pl>
2022-10-22 17:59:08 +0200

    added classes, upgrade script.js

--- js/script.js
+++ js/script.js
@@ -28,6 +28,21 @@
     const init = () => {
         render();
 
+        const form = addEventListener("submit", (event) => {
+          event.preventDefault();
+
+          const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+          if (newTaskContent === "") {
+            return;
+          }
+
+          tasks.push({
+            content: newTaskContent,
+          });
+          render();
+        });
+
     };
 
     init();
